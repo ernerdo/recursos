1c3fc25e0bb6afc6310e523072cb80374f0e3cb5
ernesto <jesh40@hotmail.es>
2017-08-30 15:59:10 -0600

    programacion add empleado

---
+++ resources/views/admin/agregar.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div id="wrapper" class="active">
+        <!-- Sidebar -->
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul id="sidebar_menu" class="sidebar-nav">
+                <li class="sidebar-brand"><a id="menu-toggle" href="#">Menu<span id="main_icon" class="glyphicon glyphicon-align-justify"></span></a></li>
+            </ul>
+            <ul class="sidebar-nav" id="sidebar">
+                <li><a href="{{ url('/catempleado') }}">test1<span class="sub_icon glyphicon glyphicon-link"></span></a></li>
+                <li><a>test2<span class="sub_icon glyphicon glyphicon-link"></span></a></li>
+            </ul>
+        </div>
+
+        <!-- Page content -->
+        <div id="page-content-wrapper">
+            <!-- Keep all page content within the page-content inset div! -->
+            <div class="page-content inset">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="well lead">Cadastro de Cliente</p>
+                        <div class="container">
+                            <div class="row"> <!-- div da esquerda -->
+
+                            </div> <!-- fim div da esquerda -->
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                 <li class="sidebar-brand"><a id="menu-toggle" href="#">Menu<span id="main_icon" class="glyphicon glyphicon-align-justify"></span></a></li>
             </ul>
             <ul class="sidebar-nav" id="sidebar">
-                <li><a>test1<span class="sub_icon glyphicon glyphicon-link"></span></a></li>
+                <li><a href="{{ url('/catempleado') }}">test1<span class="sub_icon glyphicon glyphicon-link"></span></a></li>
                 <li><a>test2<span class="sub_icon glyphicon glyphicon-link"></span></a></li>
             </ul>
         </div>
@@ -23,62 +23,7 @@
                         <p class="well lead">Cadastro de Cliente</p>
                         <div class="container">
                             <div class="row"> <!-- div da esquerda -->
-                                <!-- Text input CNPJ e Razao Social-->
-                                <div class="col-sm-8 contact-form"> <!-- div da direita -->
-                                    <form id="contact" method="post" class="form" role="form">
-                                        <div class="row">
-                                            <div class="col-xs-6 col-md-3 form-group">
-                                                <input class="form-control" id="inputCNPJ" name="CNPJ" placeholder="CNPJ" type="text" required autofocus />
-                                            </div>
-                                            <div class="col-xs-4 col-md-9 form-group">
-                                                <input class="form-control" id="inputrazaosocial" name="razaocosial" placeholder="Razão Social" type="text" />
-                                            </div>
-                                        </div> <!-- fim row -->
-                                        <!-- Text input endereco-->
-                                        <div class="col-xs-4 col-md-12 form-group">
-                                            <div class="controls">
-                                                <input class="form-control" id="inputendereco" name="endereco" placeholder="Endereço"  type="text">
-                                            </div>
-                                        </div><!--fim control-group-->
-                                        <br> <!--pulando uma linha -->
-                                        <!-- Text input cidade e estado-->
-                                        <div class="row">
-                                            <div class="col-xs-6 col-md-9 form-group">
-                                                <input class="form-control" id="inputcidade" name="cidade" placeholder="Cidade" type="text" />
-                                            </div>
-                                            <div class="col-xs-4 col-md-3 form-group">
-                                                <select class="form-control"id="selectbasic" name="selectestado" >
-                                                    <option>Selecione</option>
 
-                                                    <option>TO</option>
-                                                </select>
-                                            </div>
-                                            <div class="col-xs-6 col-md-3 form-group">
-                                                <input class="form-control" id="inputtelefone" name="telefone" placeholder="Telefone" type="text" />
-                                            </div>
-                                            <div class="col-xs-4 col-md-3 form-group">
-                                                <input class="form-control" id="inputcontato" name="contato" placeholder="Contato" type="text" />
-                                            </div>
-                                            <div class="col-xs-4 col-md-6 form-group">
-                                                <input class="form-control" id="inputemail" name="email" placeholder="E-mail" type="text" />
-                                            </div>
-                                        </div><!--fim Text input cidade e estado-->
-                                        <div class="col-xs-4 col-md-12 form-group">
-                                            <div class="controls">
-                                                <textarea class="form-control" id="message" name="message" placeholder="Message" rows="5"></textarea>
-                                            </div>
-                                        </div>
-                                        <br />
-
-                                        <div class="row">
-                                            <div class="col-xs-12 col-md-12 form-group">
-                                                <button class="btn btn-primary pull-right" type="submit">Salvar</button>
-                                                <button class="btn btn-primary pull-right" type="submit">Limpar</button>
-                                                <!--<button class="btn btn-primary pull-right" type="submit">Enviar</button>-->
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div> <!-- fim div da direita -->
                             </div> <!-- fim div da esquerda -->
                         </div>
 
