6b7ddc885fec7ebcc1a9cc7bcf930eed34d26c4d
calonzo <calonzo@MacBook-Air-de-Carlos.local>
2017-09-02 22:46:49 -0600

    Pantallas de Login y Registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.appresp')
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-4 col-md-offset-4">
+          <h2 class="text-center">Iniciar Sesión</h2>
             <div class="panel panel-default">
-                <div class="panel-heading">Iniciar Sesión</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <form class="form-signin" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo electrónico</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                            <label for="email" class="control-label">Correo electrónico</label>
+                            <div>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="user@example.com"required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -25,10 +23,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Contraseña</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                            <label for="password" class="control-label">Contraseña</label>
+                            <div>
+                                <input id="password" type="password" class="form-control" name="password" placeholder="*******"required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -39,7 +36,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div>
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordar mis datos
@@ -47,19 +44,20 @@
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Inciar
+                        <div class="form-group text-center">
+                                <button type="submit" class="btn btn-success outline btn-block">
+                                  Iniciar
                                 </button>
-
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    ¿Has olvidado tu contraseña?
+                                  ¿Has olvidado tu contraseña?
                                 </a>
-                            </div>
                         </div>
                     </form>
+                </div>
+                <div class="panel-footer text-center">
+                  <a class="btn btn-link" href="{{ route('register') }}">
+                    Solicita tu cuenta de usuario!
+                  </a>
                 </div>
             </div>
         </div>
