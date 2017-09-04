bcde195d52ba3905d20af472248da8a87e5289b7
ernesto <jesh40@hotmail.es>
2017-09-04 08:01:03 -0600

    limpieza

--- resources/views/mensajes/empleado_creado.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.dash')
-
-@section('content')
-    <div class='aprobado'><label style='color:#177F6B'>
-        <?php  echo $msj; ?>
-    </div>
-@endsection
--- resources/views/mensajes/mensaje_error.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.dash')
-
-@section('content')
-    <br/><div class='rechazado'><label style='color:#FA206A'><?php  echo $msj; ?></label>  </div>
-
-@endsection
