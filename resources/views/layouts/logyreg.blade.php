997b385e68317ff2f0f675aad99dcb55705b1dce
Carlos Alejandro Alonzo <calonzo@alvia.local>
2017-09-06 17:40:20 -0600

    Cambios pequeños

--- resources/views/layouts/logyreg.blade.php
+++ resources/views/layouts/logyreg.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
+    <link rel="icon" type="image/png" href="{{asset('img/logo.png') }}" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
